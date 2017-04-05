58a2b21ca7c961192a8f1c1461753b944a61fb84
caprepo <vijayalakshmi.david@capgemini.com>
2017-04-05 19:07:26 +0530

    display method loop added

--- MyGitApplication/src/main/java/org/cap/demo/SalesClass.java
+++ MyGitApplication/src/main/java/org/cap/demo/SalesClass.java
@@ -2,9 +2,13 @@ package org.cap.demo;
 
 public class SalesClass {
 
+	private int[] mydata={12,3,45,0,-9};
 	
 	public void display(){
 		System.out.println("Sales Class Display Method");
+		
+		for(int i=0;i<mydata.length;i++)
+			System.out.println(mydata[i]);
 	}
 	
 }
